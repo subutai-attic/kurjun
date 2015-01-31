332bc6ee6119de9ebdd43b7607676934672626d9
Azilet Beishenaliev <aziletb@gmail.com>
2015-01-31 22:39:38 +0600

    fix put operation - stream was read after being read to the end

--- file-stores/fs/src/main/java/ai/subut/kurjun/storage/fs/FileSystemFileStore.java
+++ file-stores/fs/src/main/java/ai/subut/kurjun/storage/fs/FileSystemFileStore.java
@@ -18,14 +18,20 @@ package ai.subut.kurjun.storage.fs;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,20 +39,19 @@ import org.slf4j.LoggerFactory;
 import org.apache.commons.codec.binary.Hex;
 
 import ai.subut.kurjun.model.storage.FileStore;
-import ai.subut.kurjun.storage.fs.util.FileHelpers;
 
 
 /**
  * File store backed by a local file system. Mappings of files checksums to their location paths are saved in an
  * embedded database. Actual files are saved under subdirectories of a common parent root directory specified in
- * constructor. Subdirectories are a one-letter directories that correspond to the first letter of files checksums in
- * hex format.
+ * constructor. Subdirectories are a one-letter directories that correspond to the first letter of the file name.
  */
 public class FileSystemFileStore implements FileStore
 {
-    public static final String CHECKSUM_ALGORITHM = "MD5";
+    public static final String MD5 = "MD5";
 
     private static final Logger LOGGER = LoggerFactory.getLogger( FileSystemFileStore.class );
+    private static final int BUFFER_SIZE = 1024 * 8;
 
     private Path rootLocation;
 
@@ -98,7 +103,7 @@ public class FileSystemFileStore implements FileStore
     {
         try ( InputStream is = new FileInputStream( source ) )
         {
-            String filename = Files.createTempFile( rootLocation, null, "" ).toFile().getName();
+            String filename = UUID.randomUUID().toString();
             return put( filename, is );
         }
     }
@@ -109,7 +114,7 @@ public class FileSystemFileStore implements FileStore
     {
         try ( InputStream is = source.openStream() )
         {
-            String filename = Files.createTempFile( rootLocation, null, "" ).toFile().getName();
+            String filename = UUID.randomUUID().toString();
             return put( filename, is );
         }
     }
@@ -118,34 +123,33 @@ public class FileSystemFileStore implements FileStore
     @Override
     public byte[] put( String filename, InputStream source ) throws IOException
     {
-        byte[] checksum;
-        try
-        {
-            checksum = FileHelpers.checksum( source, CHECKSUM_ALGORITHM );
-        }
-        catch ( NoSuchAlgorithmException ex )
-        {
-            LOGGER.error( "Failed to create file checksum", ex );
-            return null;
-        }
+        Objects.requireNonNull( filename, "Filename" );
+        Path target = rootLocation.resolve( filename.substring( 0, 1 ) ).resolve( filename );
+        Files.createDirectories( target.getParent() );
 
-        Path target = rootLocation.resolve( firstDigitInHex( checksum ) ).resolve( filename );
-        if ( Files.notExists( target.getParent() ) )
-        {
-            Files.createDirectories( target );
-        }
+        Path sourceDump = null;
         try ( MapDb db = new MapDb( rootLocation ) )
         {
-            // clean up target file in catch clause if copying fails
-            Files.copy( source, target, StandardCopyOption.REPLACE_EXISTING );
+            sourceDump = Files.createTempFile( rootLocation, null, null );
+            byte[] checksum = dumpStreamAndCalculateChecksum( source, sourceDump );
+
+            // clean up target file in catch clause if operation fails
+            Files.move( sourceDump, target, StandardCopyOption.REPLACE_EXISTING );
             db.getMap().put( Hex.encodeHexString( checksum ), target.toAbsolutePath().toString() );
+            return checksum;
         }
         catch ( IOException ex )
         {
             target.toFile().delete();
             throw ex;
         }
-        return checksum;
+        finally
+        {
+            if ( sourceDump != null )
+            {
+                sourceDump.toFile().delete();
+            }
+        }
     }
 
 
@@ -168,10 +172,25 @@ public class FileSystemFileStore implements FileStore
     }
 
 
-    private String firstDigitInHex( byte[] arr )
+    private byte[] dumpStreamAndCalculateChecksum( InputStream source, Path dest ) throws IOException
     {
-        String hex = Hex.encodeHexString( arr );
-        return hex.substring( 0, 1 );
+        try ( DigestInputStream is = new DigestInputStream( source, MessageDigest.getInstance( MD5 ) );
+              OutputStream os = new FileOutputStream( dest.toFile() ) )
+        {
+            int n;
+            byte[] buf = new byte[BUFFER_SIZE];
+            while ( ( n = is.read( buf ) ) > 0 )
+            {
+                os.write( buf, 0, n );
+            }
+            return is.getMessageDigest().digest();
+        }
+        catch ( NoSuchAlgorithmException ex )
+        {
+            // should not happen - handle anyways
+            throw new IOException( "Failed to open digest input stream", ex );
+        }
     }
+
 }
 
