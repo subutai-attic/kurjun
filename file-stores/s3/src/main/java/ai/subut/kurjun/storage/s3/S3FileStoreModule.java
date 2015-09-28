b9d15d5727eea798273ec29111d8ff802a4101be
Azilet Beishenaliev <aziletb@gmail.com>
2015-09-28 18:41:10 +0600

    KURJUN-37: add blueprint xml files for file store impl modules

--- file-stores/s3/src/main/java/ai/subut/kurjun/storage/s3/S3FileStoreModule.java
+++ file-stores/s3/src/main/java/ai/subut/kurjun/storage/s3/S3FileStoreModule.java
@@ -2,14 +2,9 @@ package ai.subut.kurjun.storage.s3;
 
 
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
 import com.google.inject.Module;
-import com.google.inject.Provider;
-import com.google.inject.ProvisionException;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
-import com.google.inject.name.Named;
 
 import ai.subut.kurjun.model.storage.FileStore;
 
@@ -48,38 +43,6 @@ public class S3FileStoreModule extends AbstractModule
         install( module );
 
         bind( AWSCredentials.class ).toProvider( AWSCredentialsProvider.class );
-    }
-
-
-    /**
-     * Provider class for AWS credentials. Access and secret keys are injected from properties file.
-     */
-    private static class AWSCredentialsProvider implements Provider<AWSCredentials>
-    {
-
-        @Inject( optional = true )
-        @Named( S3_ACCESS_KEY )
-        String accessKey;
-
-        @Inject( optional = true )
-        @Named( S3_SECRET_KEY )
-        String secretKey;
-
-
-        @Override
-        public AWSCredentials get()
-        {
-            if ( accessKey == null )
-            {
-                throw new ProvisionException( "No value for property " + S3_ACCESS_KEY );
-            }
-            if ( secretKey == null )
-            {
-                throw new ProvisionException( "No value for property " + S3_SECRET_KEY );
-            }
-            return new BasicAWSCredentials( accessKey, secretKey );
-        }
-
     }
 
 
