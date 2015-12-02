87932d72a5d0f8608856b2059e6e22b5d46172db
Kairat Saparbaev <s.kayrat@gmail.com>
2015-12-02 17:10:56 +0600

    KURJUN-115: Added jetty-rs module, which is standalone kurjun instance that uses REST services on embedded Jetty

---
+++ jetty-rs/src/main/java/ai/subut/kurjun/http/HttpServer.java
@@ -0,0 +1,53 @@
+package ai.subut.kurjun.http;
+
+
+import org.eclipse.jetty.server.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ai.subut.kurjun.rest.template.RestTemplateManagerImpl;
+import ai.subut.kurjun.rest.vapt.RestAptManagerImpl;
+import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+
+public class HttpServer
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger( HttpServer.class );
+
+
+    public static void main( String[] args )
+    {
+        try
+        {
+            Server server = new Server( 8081 );
+            // Configuring all static web resource
+            // final ServletHolder staticHolder = new ServletHolder(new DefaultServlet());
+
+            // Register and map the dispatcher servlet
+            final ServletHolder servletHolder = new ServletHolder( new CXFNonSpringJaxrsServlet() );
+            final ServletContextHandler context = new ServletContextHandler( ServletContextHandler.SESSIONS );
+            context.setContextPath( "/" );
+            context.addServlet( servletHolder, "/rest/kurjun/*" );
+            servletHolder.setInitParameter( "jaxrs.serviceClasses",
+                    RestAptManagerImpl.class.getName() + ","
+                    + RestTemplateManagerImpl.class.getName() );
+
+            server.setHandler( context );
+            server.start();
+            LOGGER.info( "Kurjun Jetty Server Started." );
+            server.join();
+        }
+        catch ( Exception ex )
+        {
+            LOGGER.error( "", ex );
+            System.exit( -1 );
+        }
+        finally
+        {
+            LOGGER.info( "Done!" );
+        }
+    }
+
+}
