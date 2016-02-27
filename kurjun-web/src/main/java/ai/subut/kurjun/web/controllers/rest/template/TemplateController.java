ea9fa7441b1ed7a0fb6b0ea02aa87fc5ec0077b6
Nurkaly Isakov <nisakov@critical-factor.com>
2016-02-27 18:02:33 +0600

    Created new Module Kurjun-WEB.Refactored to support Ninja-Framework

---
+++ kurjun-web/src/main/java/ai/subut/kurjun/web/controllers/rest/template/TemplateController.java
@@ -0,0 +1,10 @@
+package ai.subut.kurjun.web.controllers.rest.template;
+
+
+/**
+ * REST Controller for Template Management
+ */
+public class TemplateController
+{
+
+}
---
+++ kurjun-web/src/main/java/ai/subut/kurjun/web/controllers/rest/vapt/AptController.java
@@ -0,0 +1,9 @@
+package ai.subut.kurjun.web.controllers.rest.vapt;
+
+
+/**
+ * REST Controller for Apt Management
+ */
+public class AptController
+{
+}
