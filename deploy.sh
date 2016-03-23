bdb9eaca60dad37ffbaf42b01a1201249ff956e6
Kylych Tynybekov <ktynybekov@gmail.com>
2016-03-23 21:52:48 +0600

    kurjun with ninja standalone

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 mvn clean install -DskipTests
 
-scp -P50659 ./kurjun-web/target/original-kurjun.war jetty@158.181.221.3:/opt/jetty-9.3.8/webapps/kurjun.war
-scp -P8222 ./kurjun-web/target/original-kurjun.war jetty@54.93.172.63:/opt/jetty-9.3.8/webapps/kurjun.war
+scp -P50659 ./kurjun-web/target/kurjun.war jetty@158.181.221.3:/opt/jetty-9.3.8/webapps/kurjun.war
+scp -P8222 ./kurjun-web/target/kurjun.war jetty@54.93.172.63:/opt/jetty-9.3.8/webapps/kurjun.war
