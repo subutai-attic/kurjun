7e9c31818218ebc0ff0580ffc281145407b0c1ee
Abdysamat Mamutov <amamutov@optimal-dynamics.com>
2016-03-27 01:27:31 +0600

    Added loggin, fixed bugs.

--- kurjun-web/src/main/java/ai/subut/kurjun/web/filter/AuthorizedFilter.java
+++ kurjun-web/src/main/java/ai/subut/kurjun/web/filter/AuthorizedFilter.java
@@ -1,5 +1,8 @@
 package ai.subut.kurjun.web.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ai.subut.kurjun.model.identity.UserSession;
 import ninja.Context;
 import ninja.Filter;
@@ -12,16 +15,25 @@ import ninja.utils.NoHttpBody;
  */
 public class AuthorizedFilter implements Filter {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger( AuthorizedFilter.class );
+
     @Override
     public Result filter(FilterChain filterChain, Context context)
     {
         Result result = filterChain.next( context );
-        UserSession us = (UserSession) context.getAttribute( SecurityFilter.USER_SESSION);
-        if (us != null && us.getUser() != null
-            && !(result.getRenderable() instanceof NoHttpBody)  /* If not redirecting */
-            && Result.TEXT_HTML.equals( result.getContentType()) /* handle only html content types */)
+        try
         {
-            result.render("userInfo", us.getUser());
+            UserSession us = ( UserSession ) context.getAttribute( SecurityFilter.USER_SESSION );
+            if ( us != null && us.getUser() != null && !( result.getRenderable() instanceof NoHttpBody )  /* If not redirecting */
+
+                    && Result.TEXT_HTML.equals( result.getContentType() ) /* handle only html content types */ )
+            {
+                result.render( "userInfo", us.getUser() );
+            }
+        }
+        catch ( Exception e )
+        {
+            LOGGER.error( "Some error occurred: "+e.getMessage() );
         }
 
         return result;
--- kurjun-web/src/main/java/ai/subut/kurjun/web/security/AuthorizedUserExtractor.java
+++ kurjun-web/src/main/java/ai/subut/kurjun/web/security/AuthorizedUserExtractor.java
@@ -1,5 +1,8 @@
 package ai.subut.kurjun.web.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ai.subut.kurjun.model.identity.UserSession;
 import ai.subut.kurjun.web.filter.SecurityFilter;
 import ninja.Context;
@@ -8,11 +11,21 @@ import ninja.params.ArgumentExtractor;
 
 public class AuthorizedUserExtractor implements ArgumentExtractor<UserSession>
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger( AuthorizedUserExtractor.class );
+
     @Override
     public UserSession extract( Context context )
     {
-        UserSession uSession = (UserSession ) context.getAttribute(SecurityFilter.USER_SESSION );
-        return uSession;
+        try
+        {
+            UserSession uSession = ( UserSession ) context.getAttribute( SecurityFilter.USER_SESSION );
+            return uSession;
+        }
+        catch ( Exception e )
+        {
+            LOGGER.error( "Some error occurred: "+e.getMessage() );
+            return null;
+        }
     }
 
 
