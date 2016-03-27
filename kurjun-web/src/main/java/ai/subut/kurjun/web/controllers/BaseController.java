75dec6f70629d0514fdec060ede445aaf51b1569
Abdysamat Mamutov <amamutov@optimal-dynamics.com>
2016-03-27 10:53:09 +0600

    Added support for CORS.

--- kurjun-web/src/main/java/ai/subut/kurjun/web/controllers/BaseController.java
+++ kurjun-web/src/main/java/ai/subut/kurjun/web/controllers/BaseController.java
@@ -2,11 +2,12 @@ package ai.subut.kurjun.web.controllers;
 
 
 import ai.subut.kurjun.web.filter.AuthorizedFilter;
+import ai.subut.kurjun.web.filter.ResponseFilter;
 import ai.subut.kurjun.web.filter.SecurityFilter;
 import ninja.FilterWith;
 
 
-@FilterWith( {SecurityFilter.class, AuthorizedFilter.class })
+@FilterWith( {SecurityFilter.class, AuthorizedFilter.class, ResponseFilter.class })
 public class BaseController
 {
 
