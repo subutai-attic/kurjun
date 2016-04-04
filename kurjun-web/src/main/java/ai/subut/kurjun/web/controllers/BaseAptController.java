ede1c8741772f708c09e04bf34803b6cc1d3adaa
Abdysamat Mamutov <amamutov@optimal-dynamics.com>
2016-04-04 16:08:21 +0600

    Added properties for http/https filters and Filter classes for AptController.

--- kurjun-web/src/main/java/ai/subut/kurjun/web/controllers/BaseAptController.java
+++ kurjun-web/src/main/java/ai/subut/kurjun/web/controllers/BaseAptController.java
@@ -2,6 +2,7 @@ package ai.subut.kurjun.web.controllers;
 
 
 import ai.subut.kurjun.web.filter.AuthorizedFilter;
+import ai.subut.kurjun.web.filter.ProtocolAptFilter;
 import ai.subut.kurjun.web.filter.ResponseFilter;
 import ai.subut.kurjun.web.filter.SecurityFilter;
 import ninja.FilterWith;
@@ -10,7 +11,7 @@ import ninja.FilterWith;
 /**
  *
  */
-@FilterWith( { SecurityFilter.class, AuthorizedFilter.class, ResponseFilter.class })
+@FilterWith( { ProtocolAptFilter.class, SecurityFilter.class, AuthorizedFilter.class, ResponseFilter.class })
 public class BaseAptController
 {
     //Filter Apt REST endpoints
