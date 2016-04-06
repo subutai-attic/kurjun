e462c67a3c3e972a2cc7ce95095c0dc0d192bdce
Kylych Tynybekov <ktynybekov@gmail.com>
2016-04-06 13:19:00 +0600

    minor refactor

--- core-dao/src/main/java/ai/subut/kurjun/core/dao/service/KurjunDAOModule.java
+++ core-dao/src/main/java/ai/subut/kurjun/core/dao/service/KurjunDAOModule.java
@@ -2,17 +2,18 @@ package ai.subut.kurjun.core.dao.service;
 
 
 import com.google.inject.AbstractModule;
+import com.google.inject.persist.jpa.JpaPersistModule;
 
 import ai.subut.kurjun.core.dao.model.identity.RelationEntity;
 import ai.subut.kurjun.core.dao.model.identity.RelationObjectEntity;
+import ai.subut.kurjun.core.dao.model.identity.UserEntity;
+import ai.subut.kurjun.core.dao.model.identity.UserTokenEntity;
 import ai.subut.kurjun.core.dao.model.metadata.RepositoryArtifactEntity;
 import ai.subut.kurjun.core.dao.model.metadata.RepositoryDataEntity;
 import ai.subut.kurjun.core.dao.service.identity.IdentityDataService;
 import ai.subut.kurjun.core.dao.service.identity.IdentityDataServiceImpl;
 import ai.subut.kurjun.core.dao.service.identity.RelationDataService;
 import ai.subut.kurjun.core.dao.service.identity.RelationDataServiceImpl;
-import ai.subut.kurjun.core.dao.model.identity.UserEntity;
-import ai.subut.kurjun.core.dao.model.identity.UserTokenEntity;
 import ai.subut.kurjun.core.dao.service.metadata.RepositoryDataService;
 import ai.subut.kurjun.core.dao.service.metadata.RepositoryDataServiceImpl;
 import ai.subut.kurjun.model.identity.Relation;
@@ -33,8 +34,8 @@ public class KurjunDAOModule extends AbstractModule
     protected void configure()
     {
         //****************
-        //install( new JpaPersistModule( "PU_KURJUN" ) );
-        //bind(KurjunJPAInitializer.class).asEagerSingleton();
+        install( new JpaPersistModule( "PU_KURJUN" ) );
+        bind( KurjunJPAInitializer.class ).asEagerSingleton();
         //****************
 
         // -------------------------------------------
