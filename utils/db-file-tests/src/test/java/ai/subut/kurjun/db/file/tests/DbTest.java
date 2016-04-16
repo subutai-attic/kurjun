ae5dc63af77d35c99ae439168b0794f930ec0cd8
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-04-16 11:30:14 +0600

    fixed db bug, only first item of list was saved.

--- utils/db-file-tests/src/test/java/ai/subut/kurjun/db/file/tests/DbTest.java
+++ utils/db-file-tests/src/test/java/ai/subut/kurjun/db/file/tests/DbTest.java
@@ -4,13 +4,18 @@ package ai.subut.kurjun.db.file.tests;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.configuration.SystemConfiguration;
+import org.apache.commons.io.output.StringBuilderWriter;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,21 +24,18 @@ import org.junit.rules.TemporaryFolder;
 import org.apache.commons.io.FileUtils;
 
 import ai.subut.kurjun.db.file.FileDb;
+import ai.subut.kurjun.model.metadata.Metadata;
 import ai.subut.kurjun.model.metadata.SerializableMetadata;
 
 
-/**
- * Document me!
- */
 public class DbTest
 {
-    public static final String TARGET_PATH_PROP = "target";
-    public static final int TOTAL = 10000;
-    public static final int CONCURRENCY = 200;
-
     @Rule
     public TemporaryFolder temp = new TemporaryFolder();
 
+    private static final int TOTAL = 50;
+    private static final int MYTHREADS = 10;
+    private static final String[] MAP_NAMES = { "rawFiles", "subutaiTemplates", "aptPackages" };
 
     FileDb fileDb;
 
@@ -52,15 +54,27 @@ public class DbTest
     }
 
 
+    // test save random types of metadata
     @Test
-    public void testStress() throws Exception
+    public void testStressSaving() throws Exception
     {
-        for ( int i = 0; i < 10; i++ )
+        for ( int i = 0; i < TOTAL; i++ )
         {
             MetadataEnum type = MetadataEnum.getRandom();
             SerializableMetadata serializableMetadata = type.getSupplier().get();
 
             fileDb.put( type.getMapName(), serializableMetadata.getId(), serializableMetadata );
+        }
+    }
+
+
+    // test getting metadata
+    @Test
+    public void testStressGettingMetaData()
+    {
+        for ( final MetadataEnum metadataEnum : MetadataEnum.values() )
+        {
+            fileDb.get( metadataEnum.getMapName() );
         }
     }
 }
